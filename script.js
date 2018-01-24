014649a48d38d05414832d7920cf05bf5689de81
Amanda Camara <amandacamara@MacBook-Pro-de-Amanda.local>
2018-01-23 22:46:35 -0200

    ajustes em aula

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let titulo = document.querySelector('h1');
 let texto = document.querySelector('p');
 
 function mudarTitulo() {
-  titulo.innerHTML = 'Sua cidade';
+  titulo.innerHTML /* innerHTML significa - pegar variavel H1 e substituir no html pela seguinte frase */ = 'Venha conhecer';
 }
 
 function mudarTexto() {
-  texto.style.color = 'red';
+  texto.style.color = 'lightblue';
 }
